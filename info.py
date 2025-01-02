788c4f9decf97064b7431150c8b49d85fb6d60fb
anurag0770 <anuragsinghvlogs830@gmail.com>
2025-01-02 08:25:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://alltotalexposed830:<RPH7zHMFf22gY08F>@bots.7owui.mongodb.net/?retryWrites=true&w=majority&appName=Bots")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://alltotalexposed830:RPH7zHMFf22gY08F@bots.7owui.mongodb.net/?retryWrites=true&w=majority&appName=Bots")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
